0caad521b2bc4d4f3a43eece6df0f355f899286e
SomeSuperCoder <ithelper9@gmail.com>
2025-09-05 19:35:47 +0300

    Add message handlers

--- handers/message.go
+++ handers/message.go
@@ -1,16 +1,74 @@
 package handers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/SomeSuperCoder/global-chat/middleware"
 	"github.com/SomeSuperCoder/global-chat/models"
 	"github.com/SomeSuperCoder/global-chat/repository"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type MessageHandler struct {
 	Repo repository.MessageRepo
+}
+
+type GetResult struct {
+	Messages []models.Message `json:"messages"`
+	Count    int64            `json:"count"`
+}
+
+func (h *MessageHandler) GetMessages(w http.ResponseWriter, r *http.Request) {
+	// Prase data
+	page := r.URL.Query().Get("page")
+	limit := r.URL.Query().Get("limit")
+
+	if page == "" {
+		http.Error(w, "No page number provided", http.StatusBadRequest)
+		return
+	}
+	if limit == "" {
+		http.Error(w, "No limit number provided", http.StatusBadRequest)
+		return
+	}
+
+	pageNumber, err := strconv.Atoi(page)
+	if err != nil {
+		http.Error(w, "Invalid page number", http.StatusBadRequest)
+		return
+	}
+
+	limitNumber, err := strconv.Atoi(limit)
+	if err != nil {
+		http.Error(w, "Invalid limit number", http.StatusBadRequest)
+		return
+	}
+
+	// Fetch messages
+	messages, count, err := h.Repo.FindPaged(r.Context(), int64(pageNumber), int64(limitNumber))
+	if err != nil {
+		http.Error(w, "Failed to fetch messages", http.StatusInternalServerError)
+		return
+	}
+
+	// Make a result
+	result := &GetResult{
+		Messages: messages,
+		Count:    count,
+	}
+	resultString, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Failed to from a proper response", http.StatusInternalServerError)
+		return
+	}
+
+	// Send the result
+	fmt.Fprintln(w, string(resultString))
 }
 
 func (h *MessageHandler) CreateMessage(w http.ResponseWriter, r *http.Request) {
@@ -27,4 +85,47 @@ func (h *MessageHandler) CreateMessage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func (h *MessageHandler) DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	// Get the message ID
+	messageID := r.PathValue("id")
+	if messageID == "" {
+		panic("no message ID provided")
+	}
+
+	parsedMessageID, err := bson.ObjectIDFromHex(messageID)
+	if err != nil {
+		http.Error(w, "Invalid message ID provided", http.StatusBadRequest)
+		return
+	}
+	// Delete the message
+	h.Repo.DeleteMessage(r.Context(), parsedMessageID)
+}
+
+func (h *MessageHandler) UpdateMessageText(w http.ResponseWriter, r *http.Request) {
+	// Get the message ID
+	messageID := r.PathValue("id")
+	if messageID == "" {
+		panic("No message ID provided")
+	}
+
+	parsedMessageID, err := bson.ObjectIDFromHex(messageID)
+	if err != nil {
+		http.Error(w, "Invalid message ID provided", http.StatusBadRequest)
+		return
+	}
+
+	// Get the message text
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	messageText := string(body)
+
+	// Update the message
+	h.Repo.UpdateMessageText(r.Context(), parsedMessageID, messageText)
 }
